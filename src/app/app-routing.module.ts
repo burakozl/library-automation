06585b23ef447075babdc026c133f577833a5cbd
Burak Özel <burakk.oozel@gmail.com>
2022-11-23 15:24:54 +0300

    created routing for home and adminpanel components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminPanelComponent } from './pages/admin-panel/admin-panel.component';
+import { HomeComponent } from './pages/home/home.component';
 import { LoginRegisterComponent } from './pages/login-register/login-register.component';
 
 const routes: Routes = [
   {
+    path: "",
+    pathMatch: 'full',
+    redirectTo : 'home'
+  },
+  {
+    path: "home",
+    component : HomeComponent
+  },
+  {
+    path: "admin-panel",
+    component : AdminPanelComponent
+  },
+  {
     path: "login",
     component : LoginRegisterComponent
-  },
+  }
 ];
 
 @NgModule({
