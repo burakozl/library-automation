7a39651767a187e5f3e89285397e616ee79c211d
Burak Özel <burakk.oozel@gmail.com>
2022-11-24 22:14:39 +0300

    created method for saving books to store

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,8 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Book } from '../models/book';
+import { AppStoreState } from '../store/app.state';
+import { setBooksCartModel } from '../store/booksCart/cart.actions';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +13,16 @@ import { Book } from '../models/book';
 export class BooksService {
   private controllerUrl = `${environment.apiUrl}/books`;
 
+  booksCartModel$:Observable<Book[]>;
+
   constructor(
-    private httpClient:HttpClient
-  ) { }
+    private httpClient:HttpClient,
+    private store: Store<AppStoreState>
+  ) {
+    this.booksCartModel$ = this.store.select( //Store'dan booksCartModel'ı alıyoruz
+      (state) => state.cart.booksCartModel
+    );
+   }
 
   createBook(book:Book): Observable<Book>{
     return this.httpClient.post<Book>(`${this.controllerUrl}`,book);
@@ -32,5 +42,9 @@ export class BooksService {
 
   delete(bookId: number): Observable<void> {
     return this.httpClient.delete<void>(`${this.controllerUrl}/${bookId}`);
+  }
+
+  saveBookToStore(booksCart: Book) {
+    this.store.dispatch(setBooksCartModel( {booksCart}));
   }
 }
