cc3f94c3d098cd11e529e93b1aec39d19f056a8f
Burak Özel <burakk.oozel@gmail.com>
2022-11-25 14:30:03 +0300

    created localstorage service for saving userBooks

--- src/app/pages/books-cart/books-cart.component.ts
+++ src/app/pages/books-cart/books-cart.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Session } from 'src/app/models/session';
 import { BooksService } from 'src/app/services/books.service';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { SessionStatusService } from 'src/app/services/session-status.service';
 
 @Component({
   selector: 'app-books-cart',
@@ -9,14 +13,29 @@ import { BooksService } from 'src/app/services/books.service';
 export class BooksCartComponent implements OnInit {
 
   cartItems!:any[];
+  sessionStatus!:Session | null;
 
   constructor(
-    private booksService:BooksService
+    private booksService:BooksService,
+    private localStorageService:LocalStorageService,
+    private sessionStatusService:SessionStatusService,
+    private toastr:ToastrService
   ) { }
 
   ngOnInit(): void {
+    this.getUserInfo();
     this.getBooksFromStore();
+    if(this.localStorageService.get("lendList") === null){
+      this.localStorageService.set("lendList",'[]');
+    }
   }
+
+  getUserInfo() {
+    this.sessionStatusService.sessionStatusModel$.subscribe((res) => {
+      this.sessionStatus = res;
+    })
+  }
+
   getBooksFromStore(){
     this.booksService.booksCartModel$.subscribe((res) => {
       this.cartItems = res;
@@ -29,6 +48,25 @@ export class BooksCartComponent implements OnInit {
   }
 
   completeOrder(){
+    let lsOrder:any = localStorage.getItem('lendList');
+    let oldOrder = JSON.parse(lsOrder);
+    const newOrders:any = {
+      userName: this.sessionStatus?.name,
+      userSurname: this.sessionStatus?.surname,
+      bookInformation: this.cartItems.map(item => {
+        return {
+          ...item
+        }
+      })
+    };
+    oldOrder.push(newOrders);
+
+    this.localStorageService.set("lendList",JSON.stringify(oldOrder));
+    let ls:any = this.localStorageService.get("lendList");
+    console.log(JSON.parse(ls));
+
+    this.toastr.success("Ödünç alma işleminiz başarılı bir şekilde tamamlandı...");
+    this.booksService.deleteBookToStore();
   }
 
 }
