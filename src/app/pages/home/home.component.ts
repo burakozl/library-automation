030ec7a56117da13b0db653a041dd979d3b29557
Burak Özel <burakk.oozel@gmail.com>
2022-11-27 18:44:32 +0300

    added not available tag on cart disabled detail button

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from 'src/app/models/book';
+import { LendBooks } from 'src/app/models/lendBooks';
 import { BooksService } from 'src/app/services/books.service';
 import { DataTransferService } from 'src/app/services/data-transfer.service';
+import { LendBooksService } from 'src/app/services/lend-books.service';
 import { LoadingService } from 'src/app/services/loading.service';
 
 @Component({
@@ -17,10 +19,14 @@ export class HomeComponent implements OnInit {
   isLoading!:boolean;
   novelBooks!:Book[];
   childBooks!:Book[];
+  lendBooks!:any[];
+  notAvailableBooks!:any[];
+  isAvailable!:boolean[];
 
   constructor(
     private bookService:BooksService,
     private loadingService:LoadingService,
+    private lendBooksService:LendBooksService,
     private dataTransferService:DataTransferService//categoriyi yakalayacağımız service import et...
   ) {
     this.dataTransferService.getData().subscribe(x => {
@@ -55,10 +61,38 @@ export class HomeComponent implements OnInit {
       this.books = res;
       if(res.length > 0){
         this.getBooksByCategory();
+        this.getLendBooks();
       }
-    })
+    });
   }
 
+  getLendBooks(){
+    this.lendBooksService.getLendBooks().subscribe((res) => {
+      if(res != null) this.lendBooks = res;
+     console.log(filterByReference(this.books, this.lendBooks));
+     this.notAvailableBooks = filterByReference(this.books, this.lendBooks);
+
+     this.isAvailable = this.books.map((item) => {
+      return this.notAvailableBooks.find((book) => item.barcodeNumber === book.barcodeNumber)
+     });
+     console.log(this.isAvailable);
+
+    });
+
+
+    const filterByReference:any = (arr1:any, arr2:any) => {
+      let result = [];
+      result = arr1.filter((el:any) => {
+         return arr2.find((element:any) => {
+            return element.book_barcodeNumber === el.barcodeNumber;
+         });
+      });
+      return result;
+   }
+
+
+
+  }
 
   getBooksByCategory(){
     if(this.categaryName === 'novel'){
